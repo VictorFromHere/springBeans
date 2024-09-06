d93fbf4b2bfbea39c587c32c1e47b562e2ce8e78
VictorFromHere <fz90@yandex.ru>
2024-09-06 17:34:29 +0300

    springBeans commit

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,7 +1,10 @@
 package app;
 
 import app.config.AppConfig;
+import app.model.Animal;
 import app.model.AnimalsCage;
+import app.model.Dog;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,7 +17,7 @@ public class Application {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
+
         }
     }
-
 }
